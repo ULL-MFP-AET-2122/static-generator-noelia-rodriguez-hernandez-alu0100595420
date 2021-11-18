2dd3c2f8ea22ed8df3e82a2069a96ffbf949e1d2
NoeliaRguezHdez <alu0100595420@ull.edu.es>
2021-11-18 18:52:21 +0000

    new-deploy

---
+++ _site/_site/assets/js/mathjax-configure.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+    tex: {
+        packages: ['base', 'ams']
+    },
+    loader: {
+        load: ['ui/menu', '[tex]/ams']
+    }
+    };
+    
+    <script type="text/javascript" id="MathJax-script" async
+    
